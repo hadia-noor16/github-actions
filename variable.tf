660b744721b895c0c141ff439cd40ba1499c3f0f
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2025-10-11 18:33:32 -0600

    creating jenkins via TF

--- variable.tf
+++
@@ -1,7 +0,0 @@
-variable "vpc_cidr" {}
-variable "vpc_name" {}
-variable "cidr_public_subnet" {}
-variable "cidr_private_subnet" {}
-variable "availability_zone" {}
-variable "public_key" {}
-variable "ec2_ami" {}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_cidr" {
+    type = string
+}
+variable "vpc_name" {
+    type = string
+}
+variable "cidr_public_subnet" {
+    type = list(string)
+}
+variable "cidr_private_subnet" {
+    type = list(string)
+}
+variable "availability_zone" {
+    type = list(string)
+}
+variable "public_key" {
+    type = string
+}
+variable "ec2_ami" {
+    type = string
+}
