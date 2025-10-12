f3c570e1b74bc808fd7a820e8e19d77d58ea13df
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2025-10-11 23:38:23 -0600

    updating hosted zone file

---
+++ hosted_zone/main.tf
@@ -0,0 +1,28 @@
+variable "domain_name" {}
+variable "aws_alb_dns" {}
+variable "hosted_zone_id" {}
+
+
+output "hosted_zone_id" {
+    value= data.aws_route53_zone.dev_proj_1_mydevopslife.zone_id
+}
+
+
+data "aws_route53_zone" "dev_proj_1_mydevopslife" {
+    name = var.domain_name
+    private_zone= false
+
+}
+
+resource "aws_route53_record" "a_alias" {
+  zone_id = data.aws_route53_zone.dev_proj_1_hosted_zone_id.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_alb_dns
+    zone_id                = var.hosted_zone_id       
+    evaluate_target_health = false
+  }
+}
+
