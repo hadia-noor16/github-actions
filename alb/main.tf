2b1f12ef1d75222c0098f5546e3f8a39270d92de
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2025-10-11 23:33:29 -0600

    including ALB, TG, and ACM

---
+++ alb/main.tf
@@ -0,0 +1,69 @@
+variable "alb_name" {}
+variable "lb_type" {}
+variable "is_external" {}
+variable "sg_enable_ssh_http" {}
+variable "public_subnet_ids" {}
+variable "tag_name" {}
+variable "tg_for_lb" {}
+variable "ec2_instance_id" {}
+variable "lb_http_listener_port" {}
+variable "lb_http_listener_protocol" {}
+variable "lb_listener_default_action" {}
+variable "lb_listener_https_port" {}
+variable "lb_listener_https_protocol" {}
+variable "dev_proj_1_acm_arn" {}
+variable "tg_attachement_port" {}
+
+output "aws_alb_dns_name" {
+  value= aws_lb.dev_proj_1_alb.dns_name
+}
+
+output "aws_lb_zone_id" {
+  value = aws_lb.dev_proj_1_alb.zone_id
+}
+
+
+resource "aws_lb" "dev_proj_1_alb" {
+  name               = var.alb_name
+  internal           = var.is_external
+  load_balancer_type = var.lb_type
+  security_groups    = [var.sg_enable_ssh_http]
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = true
+
+  tags = {
+    Name= var.tag_name
+  }
+
+}
+
+resource "aws_lb_target_group_attachment" "alb_tg_attachment" {
+  target_group_arn = var.tg_for_lb
+  target_id = var.ec2_instance_id
+  port = var.tg_attachement_port
+}
+
+resource "aws_lb_listener" "dev_proj_1_lb_http_listener" {
+  load_balancer_arn = aws_lb.dev_proj_1_alb.arn
+  port = var.lb_http_listener_port
+  protocol = var.lb_http_listener_protocol
+
+  default_action {
+    type             = var.lb_listener_default_action
+    target_group_arn = var.tg_for_lb
+  }
+
+}
+
+resource "aws_lb_listener" "dev_proj_1_lb_https_listener" {
+  load_balancer_arn = aws_lb.dev_proj_1_alb.arn
+  port = var.lb_listener_https_port
+  protocol = var.lb_listener_https_protocol
+
+  default_action {
+    type             = var.lb_listener_default_action
+    target_group_arn = var.tg_for_lb
+  }
+
+}
--- main.tf
+++ main.tf
@@ -28,3 +28,45 @@ module "jenkins" {
     user_data_install_jenkins= templatefile ("./jenkins_user_data/user_data.sh", {})
 }
 
+module "lb_target_group" {
+    source = "./lb_target_group"
+    lb_target_group_name = "Target group for jenkins server"
+    lb_target_group_port = 8080
+    lb_target_group_protocol = "HTTP"
+    vpc_id= module.networking.dev_proj_1_vpc_id
+    ec2_instance_id= module.jenkins.ec2_jenkins_server
+}
+
+module "alb" {
+    source = "./alb"
+    alb_name = "dev_proj_1_alb"
+    lb_type = "application"
+    is_external = false
+    sg_enable_ssh_http = module.security_group.ec2_sg_ssh_http
+    public_subnet_ids= module.networking.public_subnet_ids
+    tag_name = "ALB for Jenkins"
+    tg_for_lb= module.lb_target_group.tg_arn
+    ec2_instance_id = module.jenkins.ec2_jenkins_server
+    lb_http_istener_port = 80
+    lb_http_listener_protocol = "HTTP"
+    lb_listener_default_action= "forward"
+    lb_listener_https_port = 443
+    lb_listener_https_protocol = "HTTPS"
+    dev_proj_1_acm_arn = module.ssl_certificate.dev_proj_1_acm_arn
+    tg_attachement_port = 80
+
+}
+
+
+module "hosted_zone" {
+    source = "./hosted_zone"
+    domain_name = "mydevopslife.com"
+    aws_alb_dns=module.alb.aws_alb_dns_name
+    hosted_zone_id= module.hosted_zone_id
+}
+
+module "ssl_certificate" {
+    source = "./ssl_certificate"
+    domain_name= "mydevopslife.com"
+    hosted_zone_id=module.hosted_zone_id
+}
